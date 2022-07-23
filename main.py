7c15ed7c02db647163dd29b9d85294e1a75f5038
vigimella <g.ciaramella98@gmail.com>
2022-07-23 21:29:38 +0200

    Code refactored, divided into several functions.

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import re
 from pydriller import RepositoryMining
 import pandas as pd
 import logging as log
-import os, shutil
+import os, git, shutil, time
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 repos_commit_csv = os.path.join(APP_ROOT, 'repos_commit_csv')
 repos_commit_csv_filtered = os.path.join(APP_ROOT, 'repos_commit_csv_filtered')
+repos_dir = os.path.join(APP_ROOT, 'repos_dir')
+commit_changes_dir = os.path.join(APP_ROOT, 'commit_changes_dir')
 
 log.basicConfig(level=log.INFO,
                 format='%(asctime)s :: proc_id %(process)s :: %(funcName)s :: %(levelname)s :: %(message)s')
@@ -25,44 +27,39 @@ def clean_directory(dir_path):
             log.info('Failed to delete %s. Reason: %s' % (file_path, e))
 
 
-def repo_analysis(csv_name):
-    df = pd.read_csv(csv_name)
+def clone_repo(repo_url, repo_dir):
+    git.Repo.clone_from(repo_url, repo_dir)
 
-    # get repo name from csv file in Repo_Name column
 
-    for num, repo in enumerate(df['Repo_Name']):
+def delete_repo(repo_dir):
+    shutil.rmtree(repo_dir)
 
-        commits_elm = list()
 
-        log.info(f'Mining {repo} (Repo n. {int(num) + 1} of {df["Repo_Name"].count()}) ...')
+def repo_mining(repo_url, repo_name, num):
+    commits_elm = list()
 
-        repo_url = 'https://github.com/' + repo
+    for commit in RepositoryMining(repo_url).traverse_commits():
 
-        for commit in RepositoryMining(repo_url).traverse_commits():
+        for m in commit.modifications:
+            commits_elm.append({
+                'Repo_Name': repo_name,
+                'Commit_URL': commit.hash,
+                'Commit_date': commit.committer_date,
+                'Author': commit.author.name,
+                'Modified_file': m.filename,
+                'Change_type': m.change_type.name,
+                'Commit Message': commit.msg
+            })
 
-            for m in commit.modifications:
-                commits_elm.append({
-                    'Repo_URL': repo_url,
-                    'Commit_URL': repo_url + '/commit/' + commit.hash,
-                    'Commit_date': commit.committer_date,
-                    'Author': commit.author.name,
-                    'Modified_file': m.filename,
-                    'Change_type': m.change_type.name,
-                    'Commit Message': commit.msg
-                })
-
-        # save csv for each repository analyzed.
-
-        commits_data = pd.DataFrame(commits_elm).set_index('Repo_URL')
-        repo_name = repo.replace('/', '_')
+        commits_data = pd.DataFrame(commits_elm)
+        repo_name = repo_name.replace('/', '_')
         csv_name = str(num) + '-commit_list-' + repo_name + '.csv'
         csv_location_path = os.path.join(repos_commit_csv, csv_name)
 
         commits_data.to_csv(csv_location_path, sep=',', encoding='utf-8')
 
 
-def save_pipeline_commits(csv_folder):
-
+def filter_commits(csv_folder):
     important_elm = list()
 
     for csv in os.listdir(csv_folder):
@@ -75,9 +72,53 @@ def save_pipeline_commits(csv_folder):
                 if re.search(check, str(row).lower()):
                     important_elm.append(df.loc[num])
 
-        new_commits_data = pd.DataFrame(important_elm).set_index('Repo_URL')
-        new_commits_data.to_csv(os.path.join(repos_commit_csv_filtered, 'filtered-' + csv), sep=',', encoding='utf-8')
+        new_commits_data = pd.DataFrame(important_elm)
+        new_commits_data.to_csv(os.path.join(repos_commit_csv_filtered, csv + '-filtered'), sep=',', encoding='utf-8')
         important_elm.clear()
+
+
+def diff_commit(csv_filtered_folder, repo_dir, repo_name, num):
+
+    for csv in os.listdir(csv_filtered_folder):
+
+        path_csv_file = os.path.join(csv_filtered_folder, csv)
+        df = pd.read_csv(path_csv_file)
+
+        csv_commit_folder = os.path.join(commit_changes_dir, repo_name.replace('/', '-'))
+
+        if not os.path.exists(csv_commit_folder):
+            os.mkdir(csv_commit_folder)
+
+        for file in df['Modified_file'].drop_duplicates():
+            cleaned_name = file.replace('.', '').replace('yml', '')
+            os.chdir(repo_dir)
+            os.system(f'git log -p -- {file} >> {csv_commit_folder}/{num}-{cleaned_name}.txt')
+            os.chdir(APP_ROOT)
+
+
+def repo_analysis(csv_name):
+    df = pd.read_csv(csv_name)
+
+    for num, repo_name in enumerate(df['Repo_Name']):
+        log.info(f'Started : {repo_name} (Repo n. {int(num) + 1} of {df["Repo_Name"].count()}) ...')
+
+        repo_url = 'https://github.com/' + repo_name
+        repo_dir = os.path.join(repos_dir, repo_name.replace('/', '-'))
+        log.info(f'Cloning {repo_name}')
+        clone_repo(repo_url, repos_dir)
+        log.info(f'Mining {repo_name}')
+        repo_mining(repo_url, repo_name, num)
+        log.info(f'Filtering {repo_name}')
+        filter_commits(repos_commit_csv)
+
+        while not os.path.isdir(os.path.join(repos_dir, repo_name.replace('/', '-'))):
+            time.sleep(30)
+            print('waiting...')
+        else:
+            log.info(f'Differences')
+            diff_commit(repos_commit_csv_filtered, repos_dir, repo_name, num)
+            log.info(f'Deleting {repo_name}')
+            shutil.rmtree(repo_dir)
 
 
 if __name__ == '__main__':
@@ -94,6 +135,14 @@ if __name__ == '__main__':
         os.makedirs(repos_commit_csv_filtered)
         log.info('Directory repos_commit_csv created...')
 
+    if not os.path.exists(repos_dir):
+        os.makedirs(repos_dir)
+        log.info('Directory repos_commit_csv created...')
+
+    if not os.path.exists(commit_changes_dir):
+        os.makedirs(commit_changes_dir)
+        log.info('Directory repos_commit_csv created...')
+
     # Cleaning directories created before
 
     clean_directory(repos_commit_csv)
@@ -102,9 +151,12 @@ if __name__ == '__main__':
     clean_directory(repos_commit_csv_filtered)
     log.info(f'{repos_commit_csv_filtered} cleaned...')
 
+    clean_directory(repos_dir)
+    log.info(f'{repos_dir} cleaned...')
+
+    clean_directory(commit_changes_dir)
+    log.info(f'{commit_changes_dir} cleaned...')
+
     # Starting process
 
     repo_analysis(csv_file)
-    log.info('Repo analysis finished')
-    log.info('Filtering commits started')
-    save_pipeline_commits(repos_commit_csv)
